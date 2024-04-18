61d5ea2c738ad68bde70003e54fae484837c7791
Ефим <fimakrut@mail.ru>
2024-04-18 16:23:49 +0500

    Разработанные контексты для моделей. Ярыгин.

---
+++ Pr26_Yar/Classes/Common/Config.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+namespace Pr26_Yar.Classes.Common
+{
+   public class Config
+    {
+        public static string ConnectionConfig = "server=10.0.201.4; uid=root; pwd=;database=pcClub;";
+        public static MySqlServerVersion Version = new MySqlServerVersion(new Version(8, 0, 11));
+    }
+}
---
+++ Pr26_Yar/Classes/UserContext.cs
@@ -0,0 +1,14 @@
+﻿using Pr26_Yar.Models;
+using Microsoft.EntityFrameworkCore;
+using Pr26_Yar.Classes.Common;
+
+namespace Pr26_Yar.Classes
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+        public UserContext() => Database.EnsureCreated();
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseMySql(Config.ConnectionConfig, Config.Version);
+    }
+}
