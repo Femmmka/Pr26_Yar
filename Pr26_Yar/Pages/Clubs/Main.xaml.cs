2ece510fe94a168b3f3cc0dc03de03afa84e50e1
Ефим <fimakrut@mail.ru>
2024-04-18 17:27:57 +0500

    Разработанный модуль управления клубами. Ярыгин

---
+++ Pr26_Yar/Pages/Clubs/Main.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Pr26_Yar.Classes;
+using System.Windows.Controls;
+
+
+namespace Pr26_Yar.Pages.Clubs
+{
+    /// <summary>
+    /// Логика взаимодействия для Main.xaml
+    /// </summary>
+    public partial class Main : Page
+    {
+            public ClubsContext AllClub = new ClubsContext();
+            public Main()
+        {
+            InitializeComponent();
+            foreach (Models.Clubs Club in AllClub.Clubs)
+                Parent.Children.Add(new Elements.Item(Club, this));
+        }
+        private void AddClub(object sender, System.Windows.RoutedEventArgs e) =>
+            MainWindow.init.OpenPages(new Pages.Clubs.Add(this));
+        
+    }
+}
